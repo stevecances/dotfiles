00ce0272d70dfa6a341b5d5cbbddfa84a0d44930
Mathieu Bigorne <mathieu.bigorne@gmail.com>
2018-09-01 08:24:46 +0200

    Simplified formulas install

---
+++ homebrew/formulas.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+brew update
+brew doctor
+brew upgrade
+
+brew install git
+brew install git-extras
+brew install git-crypt
+brew install imagemagick
+brew install maven
+brew install the_silver_searcher
+brew install wget
+brew install youtube-dl
+brew install nvm
+brew install ansible
+brew install bash-completion
+brew install jq
+brew install kubernetes-cli
+brew install kubectx
+brew install httpie
+brew install watch
+brew install htop
+brew install fzf
+brew install terminal-notifier
+brew install pv
+brew install bat
+brew install diff-so-fancy
+brew install tldr
